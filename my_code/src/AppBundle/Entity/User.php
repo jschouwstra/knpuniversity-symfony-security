ef1dc4c6e0ad5126d38b915d8ca2030b17cad6dd
jschouwstra <schouwstra.jelle@gmail.com>
2017-04-17 12:08:16 +0200

    chapter 2 added annotations for User, added migration

--- my_code/src/AppBundle/Entity/User.php
+++ my_code/src/AppBundle/Entity/User.php
@@ -10,17 +10,33 @@ namespace AppBundle\Entity;
 
 
 use Symfony\Component\Security\Core\User\UserInterface;
+use Doctrine\ORM\Mapping as ORM;
 
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="user")
+ */
 class User implements UserInterface
 {
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+    /**
+     * @ORM\Column(type="string", unique=true)
+     */
+    private $email;
     public function getUsername()
     {
-        // TODO: Implement getUsername() method.
+        return $this->email;
     }
     
     public function getRoles()
     {
-        // TODO: Implement getRoles() method.
+        return ['ROLE_USER'];
     }
 
     public function getPassword()
